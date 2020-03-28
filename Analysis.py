739f27cf684040c7ef62e04ab63db85748a49f47
Jonatas Vicente da Silva <jonatas.vicente@salvus.me>
2020-03-27 21:27:08 -0300

    Teste Primario

---
+++ Analysis.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+
+url = 'https://www.fundamentus.com.br/resultado.php'
+html = requests.get(url).content
+df_list = pd.read_html(html)
+df = df_list[-1]
+print(df)
+													###Critérios###
+#Fómula de Grahan
+#Fonte: https://www.fundamentus.com.br/pagina_do_ser/equacaoGrahan.htm
+#P = EPS Proj* (8,5 + (2*G)) * (4,4/AAA yield)
+#P = EPS Proj* (5,5 + (2*G)) * (4,4/Selic)??
+
+#-----------------------------------------------------------------------------------------------------------------------------------------#
+#Critério do fluxo de caixa descontado
+
+#-----------------------------------------------------------------------------------------------------------------------------------------#
+#Basi
+#Yield > 6%
+
+#-----------------------------------------------------------------------------------------------------------------------------------------#
+#Análise por indicadores
+								#Receita/Lucro (Lucro Líquido)
+#Setores Parentes (Resultados consistentes : baixa variação) [monopóloio ou oligopólio (Bancos, Seguro, Eletricidade, Saneamento)] {Dividendo Constante}
+#Setores Cíclicos (Resultados inconsistentes : alta variação)[Mineração, Ciderurgia, Agricultura, Logística, Transporte, Varejo, Moda]
+
+					##Formas de investir##
+#Setores Perenes: (Sócio: Viés de longo prazo){Dividendos}
+#Setores Cíclico: (Valor: Viés de Especulação)
+
+##	Análise de Setores Perenes
+#	-Lucro constante e positivo
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------#

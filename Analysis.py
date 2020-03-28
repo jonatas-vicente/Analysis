8b66ddef0913001d48ad0fc3f54b93d4ba1860a8
Jonatas Vicente da Silva <jonatas.vicente@salvus.me>
2020-03-27 21:39:30 -0300

    Formula de Graham

--- Analysis.py
+++ Analysis.py
@@ -1,16 +1,19 @@
 import requests
 import pandas as pd
 
+##----------------------------------------------------------------------------Download de Tabela----------------------------------------##
 url = 'https://www.fundamentus.com.br/resultado.php'
 html = requests.get(url).content
 df_list = pd.read_html(html)
 df = df_list[-1]
 print(df)
+
 													###Critérios###
 #Fómula de Grahan
 #Fonte: https://www.fundamentus.com.br/pagina_do_ser/equacaoGrahan.htm
 #P = EPS Proj* (8,5 + (2*G)) * (4,4/AAA yield)
 #P = EPS Proj* (5,5 + (2*G)) * (4,4/Selic)??
+#** Analisar os Índices corretos #
 
 #-----------------------------------------------------------------------------------------------------------------------------------------#
 #Critério do fluxo de caixa descontado
